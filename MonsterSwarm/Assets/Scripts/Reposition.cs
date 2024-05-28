365a9fc04cf7793d0e85ab0f73b277ea99080255
ChangHee <ckdgml586@gmail.com>
2024-05-28 17:24:29 +0900

    무한맵 제작 완료

--- MonsterSwarm/Assets/Scripts/Reposition.cs
+++ MonsterSwarm/Assets/Scripts/Reposition.cs
@@ -16,6 +16,38 @@ public class Reposition : MonoBehaviour
         if (!collision.CompareTag("Area"))
             return;
 
+        Vector3 playerPos = GameManager.instance.playerMov.transform.position;
+        Vector3 myPos = transform.position;
+        Vector3 playerDir = GameManager.instance.playerMov.moveVec;
+
+        float dirX = playerPos.x - myPos.x;
+        float dirZ = playerPos.z - myPos.z;
+
+        float differX = Mathf.Abs(dirX);
+        float differZ = Mathf.Abs(dirZ);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirZ = dirZ > 0 ? 1 : -1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(differX > differZ)
+                {
+                    transform.Translate(Vector3.right * dirX * 60);
+                }
+                else if(differX < differZ)
+                {
+                    transform.Translate(Vector3.forward * dirZ * 60);
+                }
+                else if(differZ == differX)
+                {
+                    transform.Translate(Vector3.right * dirX * 60);
+                    transform.Translate(Vector3.forward * dirZ * 60);
+                }
+                break;
+        }
+
         
     }
 }
